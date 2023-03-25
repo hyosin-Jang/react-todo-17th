6b64d3b8086639088f04a625c3f1513035c1b6a2
hyosin <>
2023-03-26 05:37:33 +0900

    feat: 중요도에 따라 글자 크기 바꾸기

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,6 +1,23 @@
 import styled from "styled-components"
-
+import React from "react"
 const TodoItem = ({item, toggleStatus, removeItem}) => {
+	const getPriorityToTagSize = (priority) => {
+		switch (priority) {
+			case "5":
+				return <h1>{item.value}</h1>
+			case "4":
+				return <h2>{item.value}</h2>
+			case "3":
+				return <h3>{item.value}</h3>
+			case "2":
+				return <h4>{item.value}</h4>
+			case "1":
+				return <h5>{item.value}</h5>
+			default:
+				return <span>{item.value}</span>
+		}
+	}
+
 	return (
 		<ItemWrapper
 			onClick={(e) => {
@@ -9,25 +26,22 @@ const TodoItem = ({item, toggleStatus, removeItem}) => {
 			key={item.id}>
 			<button
 				type="button"
-				className="button"
+				className="button icon-trash"
 				onClick={(e) => {
 					removeItem(item.id, e)
-				}}>
-				❎
-			</button>
-			<span>{item.value}</span>
+				}}></button>
+			{getPriorityToTagSize(item.priority)}
 		</ItemWrapper>
 	)
 }
 
-export default TodoItem
+export default React.memo(TodoItem)
 
 const ItemWrapper = styled.div`
 	display: flex;
 	align-items: center;
 	padding: 3px 0 3px 10px;
 	cursor: pointer;
-
 	span {
 		margin-left: 10px;
 	}
