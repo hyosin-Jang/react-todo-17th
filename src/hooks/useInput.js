106bd27c84aaad71730de61aa1151afae51924dc
hyosin <>
2023-03-26 05:38:50 +0900

    feat: useInput > onChange함수 메모이제이션

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -1,12 +1,12 @@
-import {useState} from "react"
+import {useState, useCallback} from "react"
 
 const useInput = (defaultValue) => {
 	const [value, setValue] = useState(defaultValue)
 
-	const onChange = ({target}) => {
+	const onChange = useCallback(({target}) => {
 		const {value} = target
 		setValue(value)
-	}
+	}, [])
 	return {value, onChange, setValue}
 }
 
